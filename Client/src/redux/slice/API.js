7239e2c5dccf63acaba8a115545a014032a26e66
Saqlain-313 <saqlainmalik@maxifysolution.com>
2026-07-03 18:32:20 +0530

    by malik

--- Client/src/redux/slice/API.js
+++ Client/src/redux/slice/API.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // Change in production
+  // baseURL: "http://localhost:5000/api",
+
+  baseURL:"/api",
+
   withCredentials: true,
 });
 

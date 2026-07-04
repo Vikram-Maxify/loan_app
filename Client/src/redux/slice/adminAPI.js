eeafc5aff4c38e0be4bd7d8fbd66c346ccb3ac76
Saqlain-313 <saqlainmalik@maxifysolution.com>
2026-07-04 18:20:16 +0530

    by malik

--- Client/src/redux/slice/adminAPI.js
+++ Client/src/redux/slice/adminAPI.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const adminAPI = axios.create({
-    baseURL: "http://localhost:5000/api",
+    // baseURL: "http://localhost:5000/api",
+    baseURL: "/api",
 });
 
 adminAPI.interceptors.request.use((config) => {
